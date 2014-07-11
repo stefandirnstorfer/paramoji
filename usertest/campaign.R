355c3a5c6efdce9de942d38f6d6537b505e594a5
Stefan Dirnstorfer <stefan@thetaris.com>
2014-07-11 11:28:33 +0200

    merged microworker test project

---
+++ usertest/campaign.R
@@ -0,0 +1,59 @@
+#reading the data
+require(rjson)
+data<-readLines('data/data-campaign2.json')
+data<-lapply(data, fromJSON)
+data<-data.frame(Reduce(rbind, data))
+data$Ptest=as.numeric(data$Ptest)
+data$Atest=as.numeric(data$Atest)
+data$Vtest=as.numeric(data$Vtest)
+data$Pref=as.numeric(data$Pref)
+data$Aref=as.numeric(data$Aref)
+data$Vref=as.numeric(data$Vref)
+
+training<-readLines('data/training-campaign2.json')
+training<-lapply(training, fromJSON)
+training<-data.frame(Reduce(rbind, training))
+
+# filter incomplete users
+data.uid <- as.character(data$MW_ID)
+users<-names(table(data.uid))[table(data.uid)==10]
+
+# determine quality
+data.quality2 <- (data$Vtest-data$Vref)^2 + 0.3*(data$Atest-data$Aref)^2 + 0.05*(data$Ptest-data$Pref)
+users.quality <- sapply(users, function(user) {
+  sqrt(mean(data.quality2[data.uid==user]))
+})
+users.training <- sapply(users, function(user) {
+  as.numeric(table(as.character(training$MW_ID))[user])/3
+})
+plot(users.training, users.quality, col=(users.training>2)+1)
+table(users.training)
+
+# Work speed
+users.speed <- as.numeric(sapply(users, function(user) {
+  udata <- as.character(test[data.uid==user,]$time)
+  udata <- sapply(udata, function(t) {as.POSIXct(t, format='%Y-%m-%dT%H:%M:%S')})
+  (max(udata)-min(udata))/(length(udata)-1)
+}))
+plot(users.speed, users.quality, col=(users.training>2)+1)
+
+
+# Work duration
+data.step = rep(0, nrow(test))
+for (user in users) { data.step[data.uid==user]=seq(1,10)}
+step.quality <- sapply(seq(1,10), function(step) {
+   sqrt(mean(data.quality2[data.step==step]))
+})
+plot(jitter(data.step), data.quality2, col=(users.training>2)+1, xlab='data.step')
+lines(seq(1,10), step.quality)
+
+for (i in c(1,2,3,100)) {
+d <- data[data.uid %in% users[users.training<=i], ]
+l <- data.frame(rbind(lm(d$Vref ~ d$Vtest + d$Atest + d$Ptest)$coefficients,
+lm(d$Aref ~ d$Vtest + d$Atest + d$Ptest)$coefficients,
+lm(d$Pref ~ d$Vtest + d$Atest + d$Ptest)$coefficients), 
+  row.names=c('V','A','P'))
+print(l)
+}
+
+
---
+++ usertest/run.js
@@ -0,0 +1,168 @@
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var url= require('url');
+var crypto= require('crypto');
+
+var server= http.createServer(handleRequest);
+server.listen(9009);
+
+// mime types
+var mimes= {
+    html: 'text/html',
+    css: 'text/css',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    ico: 'image/ico',
+    svg: 'image/svg+xml',
+}
+
+var workById = {};
+
+// Run web server
+function handleRequest(req, res) {
+    try {
+	var urlParts = url.parse(req.url, true);
+	var worker = urlParts.query.MW_ID;
+	var campaign = urlParts.query.CAMP_ID;
+	var workerInfo = workById[worker];
+	if (!workerInfo) {
+	    workerInfo = {
+		tasks : 0,
+		training : 0,
+		success : 0,
+		active : false
+	    };
+	    workById[worker]= workerInfo;
+	}
+
+	if (urlParts.pathname=="/") {
+	    if (!worker) {
+		denyAccess(res, "Worker ID is missing");
+	    }
+	    else if (!workerInfo.active) {
+		if (workerInfo.training < 3) {
+		    workerInfo.ref = Math.floor(Math.random()*2); 
+		    var content = {
+			"worker" : worker,
+			"campaign" : campaign,
+			"moreless" : workerInfo.ref ? 'more' : 'less',
+		    }
+		    serveFile(res, "pages/training_"+(workerInfo.training+1)+".html", content);
+		}
+		else if (workerInfo.success < 3) {
+		    delete workById[worker];
+		    serveFile(res, "pages/sorry.html", content);
+		}
+		else {
+		    workerInfo.active = true;
+		    serveFile(res, "pages/activate.html", content);
+		}
+	    }
+	    else {
+		if (workerInfo.tasks < 10) {
+		    var content = {
+			"work" : workerInfo.tasks + 1,
+			"worker" : worker,
+			"campaign" : campaign,
+			"valence" : Math.floor(Math.random()*3)/2,
+			"arousal" : Math.floor(Math.random()*3)/2,
+			"potency" : Math.floor(Math.random()*3)/2,
+		    }
+		    serveFile(res, "pages/query.html", content);
+		} else {
+		    var content = {
+			"CODE" : getVCode(campaign, worker)
+		    };
+		    serveFile(res, "pages/thankyou.html", content);
+		}
+	    }
+	} 
+	else if (urlParts.pathname=="/submit") {
+	    redirectAction(res, urlParts.query);
+	    urlParts.query.ref = workerInfo.ref;
+	    workerInfo.training += 1;
+	    if (urlParts.query.test==workerInfo.ref) {
+		workerInfo.success += 1;
+	    }
+	    writeData('data/training.json', JSON.stringify(urlParts.query)+'\n');
+	} 
+	else if (urlParts.pathname=="/submit2") {
+	    redirectAction(res, urlParts.query);
+	    writeData('data/data.json', JSON.stringify(urlParts.query)+'\n');
+	    workerInfo.tasks += 1;
+	}
+	else {
+	    serveFile(res, 'pages' + urlParts.pathname);
+	}
+    } catch (err) {
+	console.log("error occured : "+err);
+    }
+}
+
+function redirectAction(res, data) {
+    data.time = timeStamp();
+    res.writeHead(303, {"Location" : "/?MW_ID="+data.MW_ID+
+			'&CAMP_ID='+data.CAMP_ID});
+    res.end();
+}
+
+function serveFile(res, filename, content) {
+    var mime= mimes[filename.replace(/[^.]*\./g,'')];
+    if (!mime) {
+	denyAccess(res, "Access denied");
+	return;
+    }
+    fs.readFile(filename, function(err, data) {
+	if (data) {
+	    if (content) {
+		data= data.toString();
+		for (var key in content) {
+		    data= data.replace(new RegExp('{{'+key+'}}','g'), content[key]);
+		}
+	    }
+	    res.writeHead(200, { 
+		'Content-Type': mime,
+	    });
+	    res.end(data)
+	} else {
+	    res.writeHead(404, { 'Content-Type': 'text/plain' });
+	    res.end('File does not exist.');
+	}
+    });
+}
+
+function denyAccess(res, message) {
+    console.log(message);
+    res.writeHead(406, { 'Content-Type': 'text/plain' });
+    res.end(message);
+}
+
+function timeStamp() {
+    var pad= function(x) {
+	return x<10 ? '0'+x : ''+x;
+    };
+    var now= new Date();
+    return now.getFullYear()+'-'+
+	pad(now.getMonth()+1)+'-'+
+	pad(now.getDate())+'T'+
+	pad(now.getHours())+':'+
+	pad(now.getMinutes())+':'+
+	pad(now.getSeconds());
+};
+
+function getVCode(campaign, worker) {
+    var secret_key = "d1bc28bec2c909e1c8160bf50d46ead1aaccece1a67298885a47a4d3d130de46";
+    var shasum = crypto.createHash('sha256');
+    shasum.update(campaign + worker + secret_key);
+    return 'mw-'+shasum.digest('hex').replace(/(.{10})/g,'$1<wbr/>');
+}
+
+function writeData(file, data) {
+    fs.open(file, 'a', function( e, id ) {
+	fs.write( id, data, null, 'utf8', function(){
+	    fs.close(id, function(){
+	    });
+	});
+    });
+}
