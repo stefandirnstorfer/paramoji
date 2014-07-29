bd2eab7df02de77c6288cb3d2b4352d3f72332ed
Stefan Dirnstorfer <stefan@thetaris.com>
2014-07-29 10:27:53 +0200

    make base files optional

--- src/emoticons/Emoticons.scala
+++ src/emoticons/Emoticons.scala
@@ -1,130 +1,26 @@
 package emoticons
 
-import scala.xml.Node
-import scala.xml.MetaData
-import scala.xml.UnprefixedAttribute
-import scala.xml.Null
-import scala.xml.Elem
 import java.io.BufferedWriter
 import java.io.FileWriter
 import java.io.PrintWriter
 import scala.xml.PrettyPrinter
-
-class MatchException(msg : String, cause : Throwable) extends Exception(msg) {
-	def this(msg : String) = this(msg, null)
-}
-
-
-class EmoticonStructure(val node : Node) {
-	type ParameterSet = Map[String, List[Param]]
-	val children = node.child.filter { !_.label.startsWith("#") }.map { new EmoticonStructure(_) }
-
-	val attributes  = node.attributes.flatMap { attr => attr.key match {
-				case "transform" => List("transform" -> new SimpleAttributePattern(attr.value.toString))
-				case "style" => List("style" -> new StyleAttributePattern(attr.value.toString))
-				case "d" => List("d" -> new PathAttributePattern(SVGUtil.normalizePath(attr.value.toString)))
-				case _ => List()
-	}}.toList
-
-	def format_replaceAttribs(attr : MetaData, params : Iterator[Param]) : MetaData = {
-		attributes.foldLeft(attr) { case (newAttr, (key, pattern)) => 
-			val newValue= pattern.format(params);
-		  	newAttr.append(new UnprefixedAttribute(key, newValue, Null))
-		  }
-	}
-
-	def getMap() : Map[String, EmoticonStructure] = {
-		val recurse = children.map { _.getMap() }.fold(Map()) ( _ ++ _ )
-		val nodeId = node.attribute("id").map{ _.text }
-		val excluded = node.attribute("morph").exists { _.text == "fixed" } ||
-			node.prefix == "sodipodi" || node.prefix == "inkscape"
-		val excludeChildren = node.attribute("morph").exists( _.text == "fix-children")
-		if (excluded)
-		  Map()
-		else if (excludeChildren)
-		  Map(nodeId.get -> this)
-		else if (nodeId.isDefined)
-		  recurse + (nodeId.get -> this)
-		else
-		  recurse
-	}
-
-	def getParameterList(node : Node) : List[Param] = {
-	  try {
-		attributes.flatMap{ case (key, pattern) => 
-		  pattern.getParams(node.attribute(key).map( _.text).getOrElse {
-		    throw new MatchException("Missing attribute : "+key);
-		  })
-		}
-	  } catch {
-	    case e:MatchException => 
-	      val id = node.attribute("id").map(_.text).getOrElse("?")
-	      throw new MatchException(e.getMessage() + " in node #"+id, e)
-	  }
-	}
-	
-	def getParameterSet(node : Node, map : Map[String, EmoticonStructure]) : ParameterSet = {
-		val recurse = node.child
-				.filter { !_.label.startsWith("#") }
-				.map { getParameterSet(_, map) }
-				.foldLeft(Map[String, List[Param]]())(_ ++ _)
-		val nodeId = node.attribute("id").map{_.text }
-		if (nodeId.isDefined && map.contains(nodeId.get)) {
-		  recurse + (nodeId.get -> map.get(nodeId.get).get.getParameterList(node))
-		} else
-		  recurse
-	}
-	
-	def getParameterSet(otherNode : Node) : ParameterSet= {
-	    getParameterSet(otherNode, getMap());
-	}
-
-	def getParametersFromFile(file: String): ParameterSet = {
-		try {
-		  getParameterSet(EmoticonStructure.loadAsXML(file))
-		} catch {
-			case e:MatchException =>
-			throw new MatchException(file + ": "+e.getMessage, e)
-		}
-	}
-	
-	def getParametersFromFile(file : String, ref : ParameterSet) : ParameterSet = {
-	  MorphableParameter.morph(1, -1, getParametersFromFile(file), ref)
-	}
-
-	def getParametersFromFile(file : String, ref : String) : ParameterSet = {
-	  MorphableParameter.morph(1, -1, getParametersFromFile(file), getParametersFromFile(ref))
-	}
-
-	def format(param : Map[String,List[Param]]) : Node = node match {
-	case Elem(prefix, label, attribs, scope, _*) => {
-		val nodeId = node.attribute("id").map{ _.text }
-		Elem(prefix, label,
-		    if (nodeId.isDefined && param.contains(nodeId.get))
-		    	format_replaceAttribs(attribs, param(nodeId.get).iterator)
-		    else
-		        attribs.remove("morph"), 
-			scope, 
-				children.map { _.format(param) }:_*
-		)
-	}
-	case _ => node
-	}
-	
-}
-
-object EmoticonStructure {
-  	def loadAsXML(file : String) : Node = {
-		val src= scala.io.Source.fromFile(file)
-		val doc= scala.xml.parsing.XhtmlParser.apply(src)
-		doc.head
-	}
-
-  	def load(file : String) = new EmoticonStructure(loadAsXML(file))
-}
+import java.io.File
 
 object Emoticons extends App {
-	def saveEmoticon(filename : String, param : MorphableParameter.ParameterSet) {
+  val base = EmoticonStructure.load("faces/face_000.svg")
+
+  def loadParameters(code : String) = {
+	val level = 2 - code.filter(_ == '0').length
+	val file = "faces/face_"+code+".svg"
+	val baseLocal = "faces/face_base-"+level+"_"+code+".svg"
+	val baseGen = "gen/face_base-"+level+"_"+code+".svg"
+	if (new File(baseLocal).exists())
+		base.getParametersFromFile(file, baseLocal)
+	else
+		base.getParametersFromFile(file, baseGen)
+  }
+  
+  def saveEmoticon(filename : String, param : MorphableParameter.ParameterSet) {
 		println("Creating: "+filename)
 		val out = new PrintWriter(filename)
  		out.write(new PrettyPrinter(100,2).formatNodes(base.format(param)))
@@ -132,9 +28,7 @@ object Emoticons extends App {
 		out.close();
 	}
 	
-	val base = EmoticonStructure.load("faces/face_000.svg")
 	val shape_ooo= base.getParametersFromFile("faces/face_000.svg")
-	
 	saveEmoticon("gen/face_000.svg", shape_ooo)
 
 	val shape_poo= base.getParametersFromFile("faces/face_+00.svg", shape_ooo)
@@ -177,18 +71,18 @@ object Emoticons extends App {
 	saveEmoticon("gen/face_base-1_-+0.svg", emoticon(0,1,.5))
 	saveEmoticon("gen/face_base-1_--0.svg", emoticon(0,0,.5))
 
-	val shape_ppo = base.getParametersFromFile("faces/face_++0.svg", "faces/face_base-1_++0.svg")
-	val shape_pmo = base.getParametersFromFile("faces/face_+-0.svg", "faces/face_base-1_+-0.svg")
-	val shape_pop = base.getParametersFromFile("faces/face_+0+.svg", "faces/face_base-1_+0+.svg")
-	val shape_pom = base.getParametersFromFile("faces/face_+0-.svg", "faces/face_base-1_+0-.svg")
-	val shape_opp = base.getParametersFromFile("faces/face_0++.svg", "faces/face_base-1_0++.svg")
-	val shape_opm = base.getParametersFromFile("faces/face_0+-.svg", "faces/face_base-1_0+-.svg")
-	val shape_omp = base.getParametersFromFile("faces/face_0-+.svg", "faces/face_base-1_0-+.svg")
-	val shape_omm = base.getParametersFromFile("faces/face_0--.svg", "faces/face_base-1_0--.svg")
-	val shape_mpo = base.getParametersFromFile("faces/face_-+0.svg", "faces/face_base-1_-+0.svg")
-	val shape_mmo = base.getParametersFromFile("faces/face_--0.svg", "faces/face_base-1_--0.svg")
-	val shape_mop = base.getParametersFromFile("faces/face_-0+.svg", "faces/face_base-1_-0+.svg")
-	val shape_mom = base.getParametersFromFile("faces/face_-0-.svg", "faces/face_base-1_-0-.svg")
+	val shape_ppo = loadParameters("++0")
+	val shape_pmo = loadParameters("+-0")
+	val shape_pop = loadParameters("+0+")
+	val shape_pom = loadParameters("+0-")
+	val shape_opp = loadParameters("0++")
+	val shape_opm = loadParameters("0+-")
+	val shape_omp = loadParameters("0-+")
+	val shape_omm = loadParameters("0--")
+	val shape_mpo = loadParameters("-+0")
+	val shape_mmo = loadParameters("--0")
+	val shape_mop = loadParameters("-0+")
+	val shape_mom = loadParameters("-0-")
 	
 	val fixattr1 = List[String]()
 	def emoticon2(v: Double, a: Double, p: Double) = {
@@ -247,14 +141,14 @@ object Emoticons extends App {
 	saveEmoticon("gen/face_base-2_--+.svg", emoticon2(0,0,1))
 	saveEmoticon("gen/face_base-2_---.svg", emoticon2(0,0,0))
 
-	val shape_ppp = base.getParametersFromFile("faces/face_+++.svg", "faces/face_base-2_+++.svg")
-	val shape_ppm = base.getParametersFromFile("faces/face_++-.svg", "faces/face_base-2_++-.svg")
-	val shape_pmp = base.getParametersFromFile("faces/face_+-+.svg", "faces/face_base-2_+-+.svg")
-	val shape_pmm = base.getParametersFromFile("faces/face_+--.svg", "faces/face_base-2_+--.svg")
-	val shape_mpp = base.getParametersFromFile("faces/face_-++.svg", "faces/face_base-2_-++.svg")
-	val shape_mmp = base.getParametersFromFile("faces/face_--+.svg", "faces/face_base-2_--+.svg")
-	val shape_mpm = base.getParametersFromFile("faces/face_-+-.svg", "faces/face_base-2_-+-.svg")
-	val shape_mmm = base.getParametersFromFile("faces/face_---.svg", "faces/face_base-2_---.svg")
+	val shape_ppp = loadParameters("+++")
+	val shape_ppm = loadParameters("++-")
+	val shape_pmp = loadParameters("+-+")
+	val shape_pmm = loadParameters("+--")
+	val shape_mpp = loadParameters("-++")
+	val shape_mmp = loadParameters("--+")
+	val shape_mpm = loadParameters("-+-")
+	val shape_mmm = loadParameters("---")
 
 	val fixattr2 = List[String]()
 	def emoticon3(v: Double, a: Double, p: Double) = {
---
+++ src/emoticons/Structure.scala
@@ -0,0 +1,121 @@
+package emoticons
+
+import scala.xml.Node
+import scala.xml.MetaData
+import scala.xml.UnprefixedAttribute
+import scala.xml.Null
+import scala.xml.Elem
+
+class MatchException(msg : String, cause : Throwable) extends Exception(msg) {
+	def this(msg : String) = this(msg, null)
+}
+
+
+class EmoticonStructure(val node : Node) {
+	type ParameterSet = Map[String, List[Param]]
+	val children = node.child.filter { !_.label.startsWith("#") }.map { new EmoticonStructure(_) }
+
+	val attributes  = node.attributes.flatMap { attr => attr.key match {
+				case "transform" => List("transform" -> new SimpleAttributePattern(attr.value.toString))
+				case "style" => List("style" -> new StyleAttributePattern(attr.value.toString))
+				case "d" => List("d" -> new PathAttributePattern(SVGUtil.normalizePath(attr.value.toString)))
+				case _ => List()
+	}}.toList
+
+	def format_replaceAttribs(attr : MetaData, params : Iterator[Param]) : MetaData = {
+		attributes.foldLeft(attr) { case (newAttr, (key, pattern)) => 
+			val newValue= pattern.format(params);
+		  	newAttr.append(new UnprefixedAttribute(key, newValue, Null))
+		  }
+	}
+
+	def getMap() : Map[String, EmoticonStructure] = {
+		val recurse = children.map { _.getMap() }.fold(Map()) ( _ ++ _ )
+		val nodeId = node.attribute("id").map{ _.text }
+		val excluded = node.attribute("morph").exists { _.text == "fixed" } ||
+			node.prefix == "sodipodi" || node.prefix == "inkscape"
+		val excludeChildren = node.attribute("morph").exists( _.text == "fix-children")
+		if (excluded)
+		  Map()
+		else if (excludeChildren)
+		  Map(nodeId.get -> this)
+		else if (nodeId.isDefined)
+		  recurse + (nodeId.get -> this)
+		else
+		  recurse
+	}
+
+	def getParameterList(node : Node) : List[Param] = {
+	  try {
+		attributes.flatMap{ case (key, pattern) => 
+		  pattern.getParams(node.attribute(key).map( _.text).getOrElse {
+		    throw new MatchException("Missing attribute : "+key);
+		  })
+		}
+	  } catch {
+	    case e:MatchException => 
+	      val id = node.attribute("id").map(_.text).getOrElse("?")
+	      throw new MatchException(e.getMessage() + " in node #"+id, e)
+	  }
+	}
+	
+	def getParameterSet(node : Node, map : Map[String, EmoticonStructure]) : ParameterSet = {
+		val recurse = node.child
+				.filter { !_.label.startsWith("#") }
+				.map { getParameterSet(_, map) }
+				.foldLeft(Map[String, List[Param]]())(_ ++ _)
+		val nodeId = node.attribute("id").map{_.text }
+		if (nodeId.isDefined && map.contains(nodeId.get)) {
+		  recurse + (nodeId.get -> map.get(nodeId.get).get.getParameterList(node))
+		} else
+		  recurse
+	}
+	
+	def getParameterSet(otherNode : Node) : ParameterSet= {
+	    getParameterSet(otherNode, getMap());
+	}
+
+	def getParametersFromFile(file: String): ParameterSet = {
+		try {
+		  getParameterSet(EmoticonStructure.loadAsXML(file))
+		} catch {
+			case e:MatchException =>
+			throw new MatchException(file + ": "+e.getMessage, e)
+		}
+	}
+	
+	def getParametersFromFile(file : String, ref : ParameterSet) : ParameterSet = {
+	  MorphableParameter.morph(1, -1, getParametersFromFile(file), ref)
+	}
+
+	def getParametersFromFile(file : String, ref : String) : ParameterSet = {
+	  MorphableParameter.morph(1, -1, getParametersFromFile(file), getParametersFromFile(ref))
+	}
+
+	def format(param : Map[String,List[Param]]) : Node = node match {
+	case Elem(prefix, label, attribs, scope, _*) => {
+		val nodeId = node.attribute("id").map{ _.text }
+		Elem(prefix, label,
+		    if (nodeId.isDefined && param.contains(nodeId.get))
+		    	format_replaceAttribs(attribs, param(nodeId.get).iterator)
+		    else
+		        attribs.remove("morph"), 
+			scope, 
+				children.map { _.format(param) }:_*
+		)
+	}
+	case _ => node
+	}
+	
+}
+
+object EmoticonStructure {
+  	def loadAsXML(file : String) : Node = {
+		val src= scala.io.Source.fromFile(file)
+		val doc= scala.xml.parsing.XhtmlParser.apply(src)
+		doc.head
+	}
+
+  	def load(file : String) = new EmoticonStructure(loadAsXML(file))
+}
+
