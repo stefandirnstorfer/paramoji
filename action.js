6d4c48f3e2b4ed5c4dbdc4fa94db7d3c10f31401
Stefan Dirnstorfer <stefan@thetaris.com>
2021-11-10 21:31:15 +0100

    remove traces of real faces

--- action.js
+++ action.js
@@ -4,25 +4,14 @@ var state= {
     potency: 50,
     contempt: 0
 };
-var realfaces=[];
-IMG_BASE= '/emoticons/';
-
 $(async function () {
     $('input').bind('input', refresh);
     $('input').bind('change', refresh);
-    $('#emoticon').bind('click', () => setLargeFace())
 
     for (var key in state) {
         var m= location.search.match(RegExp(key.substring(0,1) + "=([0-9]+)"));
         if (m) state[key]= parseInt(m[1]);
     }
-    realfaces = await d3.csv('bestrep.csv', function(row) {
-        row.arousal = parseInt(row.arousal);
-        row.valence = parseInt(row.valence);
-        row.potency = parseInt(row.potency);
-        row.contempt = parseInt(row.contempt);
-        return row;
-    });
     redrawEmoticon();
 });
 
@@ -41,16 +30,6 @@ function setParam(v, a, p, c) {
     redrawEmoticon();
 }
 
-function setParamFromFace(index) {
-    var row= realfaces[index];
-    setParam(row.valence, row.arousal, row.potency, row.contempt)
-}
-
-function setLargeFace(flag) {
-    $('#emoticon-face').toggle(flag);
-    $('#emoticon-svg').toggle(flag === false || flag);
-}
-
 function redrawEmoticon() {
     var query= [];
     for (var key in state) {
@@ -62,25 +41,6 @@ function redrawEmoticon() {
 
     var v=state.valence, a=state.arousal, p=state.potency, c=state.contempt;
     $('#emoticon-svg').html(emoticon_svg(v, a, p, c));
-
-    var dists = realfaces.map((row,index) => ({
-        file : row.file,
-        index: index,
-        dist: Math.sqrt(
-            Math.pow(row.arousal - state.arousal, 2) +
-            Math.pow(row.valence - state.valence, 2) +
-            Math.pow(row.potency - state.potency, 2) +
-            Math.pow(row.contempt - state.contempt, 2)
-        )
-    }))
-        .sort((a,b) => a.dist - b.dist)
-        .slice(0,5)
-    d3.selectAll('.realface')
-        .data(dists)
-        .attr("style", d => 'background-image:url('+IMG_BASE+d.file+')')
-        .on('click', d => setParamFromFace(d.index))
-    d3.select('#emoticon-face')
-        .style('background-image','url('+IMG_BASE+dists[0].file+')')
 }
 
 var oldtime = undefined;
