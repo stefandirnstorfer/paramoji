067d097e972051a2921339bbfaede32dd4c97350
Stefan Dirnstorfer <stefan@thetaris.com>
2021-12-10 09:58:49 +0100

    coding length

--- doc/make_emoticons.sh
+++ doc/make_emoticons.sh
@@ -1,15 +1,16 @@
-
 set -e
+
+# Create parametric emoji
 PARAMS=$(cat *.tex | \
-   grep "emoticon{" | \
-   sed -e s"/.*emoticon{\([^}]*\)}.*/\1/" | \
+   grep "paramoji{" | \
+   sed -e s"/.*paramoji{\([^}]*\)}.*/\1/" | \
    sort -u)
 
 for PARAM in $PARAMS
 do
 FILE=gen/emoticon-$PARAM.pdf
 if [ ! -e $FILE ]; then
-echo Processing emoticon with params=$PARAM
+echo Processing parametric emoji with params=$PARAM
 mkdir -p gen
 node << EOF | inkscape --pipe --export-filename=$FILE
 const emoji = require('./emoticon.js')
@@ -18,7 +19,24 @@ EOF
 fi
 done
 
+# Create unicode emoji based on noto font
+NOTO_PATH=../../noto-emoji/svg
+PARAMS=$(cat *.tex | \
+   grep "notomoji{" | \
+   sed -e s"/.*notomoji{\([^}]*\)}.*/\1/" | \
+   sort -u)
 
-if [ ! -e gen/emojito.pdf]; then
+for PARAM in $PARAMS
+do
+FILE=gen/emoticon-u$PARAM.pdf
+if [ ! -e $FILE ]; then
+  echo Processing noto emoji with params=$PARAM
+  mkdir -p gen
+  inkscape $NOTO_PATH/emoji_u$PARAM.svg --export-filename=$FILE
+fi
+done
+
+
+if [ ! -e gen/emojito.pdf ]; then
 inkscape emojito.svg --export-filename=gen/emojito.pdf
 fi
