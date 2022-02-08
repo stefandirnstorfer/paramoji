9dfacbf21f244632ab70908fbfd888cbbf71c517
Stefan Dirnstorfer <stefan@thetaris.com>
2022-02-08 14:18:13 +0100

    affnet task generation

--- crowdwork/src/EmoticImage.vue
+++ crowdwork/src/EmoticImage.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 svg(width="100%" height="100%" viewBox="-2 -2 404 404")
-    image(:width="item.Width" :height="item.Height" :href="url()" preserveAspectRatio="none")
-    rect(:x="item.X_min" :y="item.Y_min"
+    image(:width="width" :height="height" :href="url()" :preserveAspectRatio="aspectRatio")
+    rect(v-if="item.X_min" :x="item.X_min" :y="item.Y_min"
          :width="(item.X_max - item.X_min)"
          :height="(item.Y_max - item.Y_min)"
-         fill="none" stroke="red" :stroke-width="strokewidth()")
+         fill="none" stroke="red" :stroke-width="strokewidth")
 </template>
 
 <script>
@@ -19,13 +19,15 @@ export default {
   },
   methods: {
     update() {
-      let vb=''
-      if (!this.rect) {
-        vb = (this.item.X_min - 5) + ' ' + (this.item.Y_min - 5 ) + ' ' +
+      let vb = ''
+      if (!this.item.Width && !this.item.X_min) {
+        vb = "0 0 100 100"
+      } else if (!this.rect) {
+        vb = (this.item.X_min - 5) + ' ' + (this.item.Y_min - 5) + ' ' +
             (this.item.X_max - this.item.X_min + 10) + ' ' +
             (this.item.Y_max - this.item.Y_min + 10)
       } else {
-        vb = '-2 -2 '+(this.item.Width + 2)+' '+(this.item.Height + 2)
+        vb = '-2 -2 ' + (this.item.Width + 2) + ' ' + (this.item.Height + 2)
       }
       this.$el.setAttribute('viewBox', vb)
     },
@@ -33,12 +35,17 @@ export default {
       return this.item.url ||
           (BASE_URL + "/emoticon-data/" + this.item.file)
     },
+  },
+  computed: {
     strokewidth() {
       if (this.rect)
-        return this.item.Height/150
+        return this.item.Height / 150
       else
-        return (this.item.Y_max - this.item.Y_min)/100
-    }
+        return (this.item.Y_max - this.item.Y_min) / 100
+    },
+    width() { return this.item.Width || 100 },
+    height() { return this.item.Height || 100 },
+    aspectRatio() { return this.width && this.height ? "none" : "xMidYMid" }
   },
   watch: {
     item() { this.update() }
