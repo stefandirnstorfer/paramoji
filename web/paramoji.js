3b5b6ade55bcbb386c90a152a24f4144f8dddb8b
Stefan Dirnstorfer <stefan@thetaris.com>
2023-10-21 09:44:16 +0200

    unify eye clip

--- web/paramoji.js
+++ web/paramoji.js
@@ -61,13 +61,6 @@ function paramoji_svg(v, a1, a2, p, c) {
         [  5,  0, 12,  0], [  0, -9,-11,  0],
         [ -4,  0,  5,  0], [  0, -6,-13,  0],
         [  0,  0,-11,  0],
-    // id: lens
-    //   transform="translate(?,?)"
-        [  0,  0,  0,  0, -1, -3], [  0,-2.5,  0,  0,-1.5],
-    //   r="?"
-        [0.5,  0,  5,  0],
-    //   d="M-15,-20H20V0Q?,? -15,-2Z"
-        [  5,  1, -4,  0,  8], [  2, -9,-17,  0,  5],
     // id: eye-l
     //   transform="matrix(-1,0,0,?,0,0)"
         [  0, 0,  0,  0, 26],
@@ -86,10 +79,16 @@ function paramoji_svg(v, a1, a2, p, c) {
         [  4,  0, -5,  0], [  0, -6,-13,  0],
         [  0,  0, -11,  0],
         [  1, 0, 0,0,0, 1],
+    // id: lens
+    //   transform="translate(?,?)"
+        [  0,  0,  0,  0, -1, -4], [  0,-2.5,  0,  0,-1.5],
+    //   r="?"
+        [0.5,  0,  5,  0],
     //   x="?"
-        [  0,  0,  0,  0,  2],
-    //   d="M-15,-20H15V-2Q?,? -20,0Z"
-        [  2,  1,-10,  0, -6,  3], [  2, -9,-17,  0,  4, 15],
+        [  -36,  0,  0,  0,  2],
+    //   d="M-15,-20H20V0Q?,? -15,-2Z M-15,-20H15V-2Q?,? -20,0Z"
+        [  5,  1, -4,  0,  8], [  2, -9,-17,  0,  5],
+        [  -34,  1,-10,  0, -6,  3], [  2, -9,-17,  0,  4, 15],
     //   d="M?,?Q?,? ?,?"  stroke-width="?"
         [ 59,  2, -1,  0, -6], [ 40, -7,-19,  0, 12],
         [ 70,  1,  2,  0, -5], [ 40,-12,-21,  0,  3],
@@ -106,13 +105,12 @@ function paramoji_svg(v, a1, a2, p, c) {
     const template= [
         '<svg width="100%" height="100%" viewBox="0 0 100 100">',
         '  <defs>',
-        '    <clipPath id="c-eye-r"><use href="#eye-r"/></clipPath>',
-        '    <clipPath id="c-eye-l"><use href="#eye-l"/></clipPath>',
-        '    <clipPath id="c-lips"><use href="#lips"/></clipPath>',
+        '    <clipPath id="clip-eyes"><use href="#eye-l"/><use href="#eye-r"/></clipPath>',
+        '    <clipPath id="clip-lips"><use href="#lips"/></clipPath>',
         '  </defs>',
         '  <path transform="matrix(1,?,0,1,53,?)" d="M-4,0q-2,-2 -4,0M-1,0Q3,-2 4,-1T6,-3 4,?" fill="none" stroke="black"/>',
         '  <path d="M?,?Q?,? ?,? M?,?Q?,? ?,?" fill="none" stroke="black"/>',
-        '  <g clip-path="url(#c-lips)">',
+        '  <g clip-path="url(#clip-lips)">',
         '    <rect height="100" width="100"/>',
         '    <ellipse cx="50" cy="91" rx="15" ry="10" fill="#800f08"/>',
         '    <g transform="translate(0,?)">',
@@ -131,20 +129,15 @@ function paramoji_svg(v, a1, a2, p, c) {
         '  <path id="lips" d="M?,?C?,? ?,? 0,?S?,? ?,?C?,? ?,? 0,?S?,? ?,?Z" transform="matrix(1,?,0,1,?,?)" fill="none" stroke="black"/>',
         '  <g transform="translate(68,?)">',
         '    <path id="eye-r" transform="rotate(?)" d="M-8,?C?,? ?,? ?,? ?,? ?,? ?,0 ?,? ?,? -8,?Z" fill="white" stroke="black"/>',
-        '    <g clip-path="url(#c-eye-r)">',
+        '    <path id="eye-l" transform="translate(-36,0) rotate(?) scale(1,?) rotate(?)" d="M8,?C?,? ?,? ?,? ?,? ?,? ?,0 ?,? ?,? 8,?Z" fill="white" stroke="black" stroke-width="?"/>',
+        '    <g clip-path="url(#clip-eyes)">',
         '      <g id="lens" transform="translate(?,?)">',
-        '      <circle r="5" fill="#9d4922"/>',
-        '      <circle r="?"/>',
-        '      <circle r="1" cx="-2.5" cy="-1.5" fill="white"/>',
+        '        <circle r="5" fill="#9d4922"/>',
+        '        <circle r="?"/>',
+        '        <circle r="1" cx="-2.5" cy="-1.5" fill="white"/>',
         '      </g>',
-        '      <path d="M-25,-40H20V0Q?,? -15,-2Z" opacity=".25"/>',
-        '    </g>',
-        '    <g transform="translate(-36,0)">',
-        '      <path id="eye-l" transform="rotate(?) scale(1,?) rotate(?)" d="M8,?C?,? ?,? ?,? ?,? ?,? ?,0 ?,? ?,? 8,?Z" fill="white" stroke="black" stroke-width="?"/>',
-        '      <g clip-path="url(#c-eye-l)">',
-        '        <use href="#lens" x="?"/>',
-        '        <path d="M-50,-40H15V-2Q?,? -20,0Z" opacity=".25"/>',
-        '      </g>',
+        '      <use href="#lens" x="?"/>',
+        '      <path d="M-70,-30H30L20,0Q?,? -18,-2 ?,? -56,0Z" opacity=".25"/>',
         '    </g>',
         '  </g>',
         '  <g id="brow-r">',
