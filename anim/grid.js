cdff438ecbe8ea090bb169fb3a5580f84efc2ac7
Stefan Dirnstorfer <stefan@thetaris.com>
2025-03-15 21:06:10 +0100

    new grid

--- anim/grid.js
+++ anim/grid.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
-const emoji = require('../doc/emoticon.js')
+import fs from 'fs'
+import { paramoji_svg, dark_mode } from '../web/paramoji.js'
 
-center = [50,50,50,50,0]
+const cols= 6
+const rows= 6
+const topLeft = [20,70,20,100,20,0,0]
+const topRight = [20,20,0,30,20,40,0]
+const bottomLeft = [100,50,50,40,0,0,0]
+const bottomRight = [100,20,80,70,0,30,0]
 
-cols= 6
-rows= 4
-dim1 = [100, 0, 0, 0, 0]
-dim2 = [0, 0, 0, 100, 0]
-
-function lin(x, y, z, cy, cz) {
-    return x.map((a,i) => a + y[i]*(cy) + z[i]*(cz))
+function bilin(t, a, b) {
+    return a.map((v, i) => v*(1-t) + b[i]*t)
 }
 
 const width = 100 * cols
@@ -17,11 +17,14 @@ const height = 100 * rows
 let svg = '<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="'+width+'" height="'+height+'">\n'
 for (let row=0; row<rows; ++row) {
     for (let col=0; col<cols; ++col) {
-        const params = lin(
-            center, dim1, dim2,row/(rows-1)-.5,col/(cols-1)-.5
+        const t1 = row/(rows-1),
+            t2 =col/(cols-1)
+        const params = bilin(t1,
+            bilin(t2, topLeft, topRight),
+            bilin(t2, bottomLeft, bottomRight)
         )
         const id_prefix = row+","+col+":"
-        const e = emoji.emoticon_svg(params[0],params[1],params[2], params[3],params[4],1)
+        const e = paramoji_svg(...params.map(x => x/100))
             .replace(/<\/?svg[^>]*>/g,'')
             .replace(/href="#/g,"href=\"#id-"+id_prefix+"-")
             .replace(/\(#/g,"(#id-"+id_prefix+"-")
@@ -29,7 +32,7 @@ for (let row=0; row<rows; ++row) {
             //.replace(/(<(?!rect id="tooth")[^>]*) stroke="black"/g, '$1 stroke="white"')
         const x= col * 100, y=row*100
         svg += '<g transform="translate('+x+','+y+')">\n'
-        svg += e+'</g>\n'
+        svg += dark_mode(e)+'</g>\n'
     }
 }
 svg += '</svg>\n'
