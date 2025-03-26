1290eafff564145a0d55cba8e02ec4af9d303d3b
Stefan Dirnstorfer <stefan@thetaris.com>
2025-03-26 12:43:35 +0100

    checkers grid

--- anim/grid.js
+++ anim/grid.js
@@ -3,22 +3,24 @@ import { paramoji_svg, dark_mode } from '../web/paramoji.js'
 
 const cols= 6
 const rows= 6
-const topLeft = [20,70,20,120,20,0,0]
-const topRight = [20,20,0,30,20,40,0]
-const bottomLeft = [70,50,50,20,0,0,0]
-const bottomRight = [100,0,80,70,0,30,0]
+const bottomLeft = [20,70,20,120,20,0,0]
+const bottomRight = [20,20,0,30,20,40,0]
+const topLeft = [70,50,50,20,0,0,0]
+const topRight = [100,0,80,70,0,30,0]
 
 function bilin(t, a, b) {
     return a.map((v, i) => v*(1-t) + b[i]*t)
 }
 
-const cellHeight = 75, cellWidth = 80
-const width = cellWidth * (cols -1) + 100
-const height = cellHeight * (rows -1) + 100
+const cellHeight = 76, cellWidth = 80
+const width = cellWidth * cols
+const height = cellHeight * rows
 let svg = '<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="'+width+'" height="'+height+'">\n'
-svg += '<rect width="'+width+'" height="'+height+'" fill="black"/>\n'
 for (let row=0; row<rows; ++row) {
     for (let col=0; col<cols; ++col) {
+        const x= col * cellWidth, y=row*cellHeight
+        const color = (row+col)%2 ? 'white' : 'black'
+        svg += `<rect x="${x}" y="${y}" width="${width}" height="${height}" fill="${color}"/>\n`
         const t1 = row/(rows-1),
             t2 =col/(cols-1)
         const params = bilin(t1,
@@ -31,11 +33,10 @@ for (let row=0; row<rows; ++row) {
             .replace(/href="#/g,"href=\"#id-"+id_prefix+"-")
             .replace(/\(#/g,"(#id-"+id_prefix+"-")
             .replace(/id="/g,"id=\"id-"+id_prefix+"-")
-        e = dark_mode(e)
-        const x= col * cellWidth, y=row*cellHeight
-        svg += '<g transform="translate('+x+','+y+')">\n'
+        if (color=='black') e = dark_mode(e)
+        svg += `<g transform="translate(${x-10},${y-17})">\n`
         svg += e + '</g>\n'
     }
 }
 svg += '</svg>\n'
-fs.writeFileSync('emotions.svg', svg)
+fs.writeFileSync('checkers.svg', svg)
